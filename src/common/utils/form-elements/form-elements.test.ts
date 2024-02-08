e2f4c5f454c7b40c97953fa48ea22454bcbb5af9
milenacrios <milena.rios@mail.uft.edu.br>
2024-02-08 14:27:27 -0300

    feat: unit tests for the functions getFormElements and getFormValues

--- src/common/utils/form-elements/form-elements.test.ts
+++ src/common/utils/form-elements/form-elements.test.ts
@@ -1,38 +1,78 @@
-import { MalgaFormElements } from 'src/common/interfaces'
 import { getFormElements } from './form-elements'
+import {
+  formElementsMock,
+  handleFormMock,
+  setAttributeFormElements,
+} from '../../../../tests/mocks/formElements-mock'
+describe('getFormElements', () => {
+  test('should be returned the elements correctly', () => {
+    const {
+      form,
+      holderNameInput,
+      cvvInput,
+      expirationDateInput,
+      numberInput,
+    } = handleFormMock()
 
-const formElementsMock: MalgaFormElements = {
-  form: 'data-malga-tokenization-feeorm',
-  holderName: 'data-malga-tokenization-holder-name',
-  number: 'data-malga-tokenization-number',
-  expirationDate: 'data-malga-tokenization-expiration-date',
-  cvv: 'data-malga-tokenization-cvv',
-}
+    form.setAttribute(formElementsMock.form, '')
+    holderNameInput.setAttribute(formElementsMock.holderName, '')
+    numberInput.setAttribute(formElementsMock.number, '')
+    cvvInput.setAttribute(formElementsMock.cvv, '')
+    expirationDateInput.setAttribute(formElementsMock.expirationDate, '')
 
-beforeEach(() => {
-  const div = document.createElement('div')
-  div.innerHTML = `
-    <div>
-      <form data-malga-tokenization-form>
-        <input data-malga-tokenization-holder-name type="text" />
-        <input data-malga-tokenization-number type="text" />
-        <input data-malga-tokenization-expiration-date type="text" />
-        <input data-malga-tokenization-cvv type="text" />
-      </form>
-    </div>
-    
-  `
-  document.body.appendChild(div)
-})
+    document.body.appendChild(form)
+    form.appendChild(holderNameInput)
+    form.appendChild(numberInput)
+    form.appendChild(expirationDateInput)
+    form.appendChild(cvvInput)
 
-test('should be returned the elements correctly', () => {
-  const formElements = getFormElements(formElementsMock)
+    const formElements = getFormElements({
+      form: 'data-malga-tokenization-form',
+      holderName: 'data-malga-tokenization-holder-name',
+      number: 'data-malga-tokenization-number',
+      expirationDate: 'data-malga-tokenization-expiration-date',
+      cvv: 'data-malga-tokenization-cvv',
+    })
 
-  console.log(formElements)
+    expect(formElements.form).toBe(form)
+    expect(formElements.holderName).toBe(holderNameInput)
+    expect(formElements.number).toBe(numberInput)
+    expect(formElements.expirationDate).toBe(expirationDateInput)
+    expect(formElements.cvv).toBe(cvvInput)
+  })
 
-  expect(formElements.form).toBeInTheDocument()
-  expect(formElements.holderName).toBeInTheDocument()
-  expect(formElements.cvv).toBeInTheDocument()
-  expect(formElements.expirationDate).toBeInTheDocument()
-  expect(formElements.number).toBeInTheDocument()
+  test("Should be returned null when elements aren't finded", () => {
+    setAttributeFormElements()
+
+    const formElements = getFormElements({
+      form: 'data-malga-tokenization-form',
+      holderName: 'data-malga-tokenization-holder-name',
+      number: 'data-malga-tokenization-number',
+      expirationDate: 'data-malga-tokenization-expiration-date',
+      cvv: 'data-malga-tokenization-cvv',
+    })
+
+    expect(formElements.form).toBeInTheDocument()
+    expect(formElements.holderName).toBeInTheDocument()
+    expect(formElements.cvv).toBeInTheDocument()
+    expect(formElements.expirationDate).toBeInTheDocument()
+    expect(formElements.number).toBeInTheDocument()
+  })
+
+  test("Should be returned null when elements aren't finded", () => {
+    setAttributeFormElements()
+
+    const formElements = getFormElements({
+      form: 'data-malga-form',
+      holderName: 'data-malga-name',
+      number: 'data-malga-number',
+      expirationDate: 'data-malga-expiration-date',
+      cvv: 'data-malga-cvv',
+    })
+    expect(formElements.form).toBeNull()
+    expect(formElements.holderName).toBeNull()
+    expect(formElements.number).toBeNull()
+    expect(formElements.expirationDate).toBeNull()
+    expect(formElements.cvv).toBeNull()
+  })
 })
--- src/common/utils/form-values/form-values.test.ts
+++ src/common/utils/form-values/form-values.test.ts
@@ -1,40 +1,54 @@
-import { MalgaFormElements } from 'src/common/interfaces'
+import {
+  appendValueOnInputs,
+  formElementsMock,
+  handleFormMock,
+  setAttributeFormElements,
+} from '../../../../tests/mocks/formElements-mock'
 import { getFormValues } from './form-values'
-
-const formElementsMock: MalgaFormElements = {
-  form: 'data-malga-tokenization-form',
-  holderName: 'data-malga-tokenization-holder-name',
-  number: 'data-malga-tokenization-number',
-  expirationDate: 'data-malga-tokenization-expiration-date',
-  cvv: 'data-malga-tokenization-cvv',
-}
-
-beforeEach(() => {
-  const div = document.createElement('div')
-  div.innerHTML = `
-    <form data-malga-tokenization-form>
-        <input data-malga-tokenization-holder-name type="text" />
-        <input data-malga-tokenization-number type="text" />
-        <input data-malga-tokenization-expiration-date type="text" />
-        <input data-malga-tokenization-cvv type="text" />
-    </form>
-    `
-  document.body.appendChild(div)
-
-  const inputs = div.querySelectorAll('input')
-  inputs[0].setAttribute('value', 'Milena Rios')
-  inputs[1].setAttribute('value', '5173 0002 6586 0114')
-  inputs[2].setAttribute('value', '05/08/2024')
-  inputs[3].setAttribute('value', '114')
-})
 
 describe('getFormValues', () => {
   test('should be the values of elements equals to sended', () => {
+    setAttributeFormElements()
+    appendValueOnInputs()
     const formValue = getFormValues(formElementsMock)
 
     expect(formValue.holderName).toEqual('Milena Rios')
     expect(formValue.number).toEqual('5173 0002 6586 0114')
     expect(formValue.expirationDate).toEqual('05/08/2024')
     expect(formValue.cvv).toEqual('114')
+  })
+  test("should be returned undefined when elements aren't finded", () => {
+    const {
+      form,
+      holderNameInput,
+      cvvInput,
+      expirationDateInput,
+      numberInput,
+    } = handleFormMock()
+
+    form.setAttribute(formElementsMock.form, '')
+    holderNameInput.setAttribute(formElementsMock.holderName, '')
+    numberInput.setAttribute(formElementsMock.number, '')
+    cvvInput.setAttribute(formElementsMock.cvv, '')
+    expirationDateInput.setAttribute(formElementsMock.expirationDate, '')
+
+    document.body.appendChild(form)
+    form.appendChild(holderNameInput)
+    form.appendChild(numberInput)
+    form.appendChild(expirationDateInput)
+    form.appendChild(cvvInput)
+
+    const inputs = document.querySelectorAll('input')
+    inputs[0].value = ''
+    inputs[1].value = ''
+    inputs[2].value = ''
+    inputs[3].value = ''
+
+    const formValue = getFormValues(formElementsMock)
+
+    expect(formValue.holderName).toBe('')
+    expect(formValue.number).toBe('')
+    expect(formValue.expirationDate).toBe('')
+    expect(formValue.cvv).toBe('')
   })
 })
