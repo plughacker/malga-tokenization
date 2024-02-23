41e27778293d322265b28dd4aef2442da9356e9f
milenacrios <milena.rios@mail.uft.edu.br>
2024-02-23 17:54:27 -0300

    fix: test correction to throw error in form-values.test

--- src/common/utils/tests/form-values.test.ts
+++ src/common/utils/tests/form-values.test.ts
@@ -86,4 +86,41 @@ describe('getFormValues', () => {
     expect(formValue.expirationDate).toBe('')
     expect(formValue.cvv).toBe('')
   })
+  test('should be possible to return error if the elements are not found', async () => {
+    const {
+      form,
+      holderNameInput,
+      cvvInput,
+      expirationDateInput,
+      numberInput,
+    } = handleFormMock()
+
+    form.setAttribute(formElementsMock.form, '')
+    holderNameInput.setAttribute(formElementsMock.holderName, '')
+    numberInput.setAttribute(formElementsMock.number, '')
+    cvvInput.setAttribute(formElementsMock.cvv, '')
+    expirationDateInput.setAttribute(formElementsMock.expirationDate, '')
+
+    document.body.appendChild(form)
+    form.appendChild(holderNameInput)
+    form.appendChild(numberInput)
+    form.appendChild(expirationDateInput)
+    form.appendChild(cvvInput)
+
+    const inputs = document.querySelectorAll('input')
+    inputs[0].value = formValuesMock.holderName
+    inputs[1].value = formValuesMock.number
+    inputs[2].value = formValuesMock.expirationDate
+    inputs[3].value = formValuesMock.cvv
+
+    expect(() =>
+      getFormValues({
+        form: 'data-malga-form',
+        holderName: 'data-malga-holder-name',
+        number: 'data-malga-number',
+        expirationDate: 'data-malga-expiration-date',
+        cvv: 'data-malga-cvv',
+      }),
+    ).toThrowError()
+  })
 })
