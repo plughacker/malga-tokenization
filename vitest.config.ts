9cc1bfb550fbfc605454fd0b2de79e3fda308595
Leonardo Pinheiro <leoprocha100@gmail.com>
2024-03-01 15:21:09 -0300

    test(configs): adding vite tsconfig paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [viteTsConfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
