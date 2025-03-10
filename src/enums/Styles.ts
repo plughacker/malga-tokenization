eb17935777beca9521520938627ed6626a890a83
lariane-guerra <larianespalenza@gmail.com>
2025-03-10 14:23:07 -0300

    feat(workflow): try to use changesets

--- src/enums/Styles.ts
+++ src/enums/Styles.ts
@@ -2,5 +2,5 @@ export enum CSSClasses {
   Invalid = 'malga-hosted-field-invalid',
   Valid = 'malga-hosted-field-valid',
   Focused = 'malga-hosted-field-focused',
-  Default = 'malga-hosted-field',
+  Default = 'malga-hosted-field-default',
 }
